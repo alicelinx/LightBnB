eceecba07ee4dd5e6148851193a5c7d2d5dbb6d8
Alice Lin <alicexxlin@gmail.com>
2023-06-29 19:59:21 +0000

    refactor getAllReservations

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -68,7 +68,22 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating, properties.thumbnail_photo_url, properties.parking_spaces, properties.number_of_bathrooms, properties.number_of_bedrooms
+    FROM property_reviews
+    JOIN properties ON property_id = properties.id
+    JOIN reservations ON reservation_id = reservations.id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2`, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Properties
